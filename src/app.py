ab2f04b28024a9ab52f3c06aa98cbbe766bb9812
Britney Escoffery <britneybescoffery@gmail.com>
2025-09-19 23:31:57 +0000

    completed get, put, and delete methods

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Event
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,57 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    users = User.query.all()
+    users_list = [item.serialize() for item in users]
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ",
+        "data": users_list
     }
 
     return jsonify(response_body), 200
+
+@app.route('/event', methods=['GET'])
+def event_column():
+    all_events = Event.query.all()
+    event_list = [item.serialize() for item in all_events]
+    
+    return jsonify (event_list), 200
+
+@app.route('/event', methods=['POST'])
+def event_add():
+    body = request.get_json()
+    name_data = body['name']
+    event_planner_data = body['event_planner']
+    new_event = Event(
+        name = name_data, 
+        event_planner = event_planner_data
+    )
+    db.session.add(new_event)
+    db.session.commit()
+
+    return jsonify(new_event.serialize()), 200
+
+@app.route('/event/<int:event_id>', methods=['PUT'])
+def update_event(event_id):
+    body = request.get_json()
+    event = db.session.get(Event, event_id)
+    if event: 
+
+        print ("event tag!!!!!!:",event)
+        event.name = body["name"]
+        db.session.commit()
+
+        return jsonify(event.serialize()), 200
+    else:
+        return "no event with that id!!!!"
+
+@app.route('/event/<int:event_id>', methods=['DELETE'])
+def event_delete(event_id):
+    event = db.session.get(Event, event_id)
+    db.session.add(event)
+    db.session.commit()
+    
+    return jsonify(event.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Column, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False) 
+    password = Column(String(120), nullable=False)
+    is_active = Column(Boolean(), nullable=False)
+    events_organized = relationship('Event', backref = "event_post", lazy = True)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+class Event(db.Model):
+    id = Column(Integer, primary_key = True)
+    name = Column(String(120), unique = False, nullable = False)
+    time = Column(DateTime, default = datetime.now(timezone.utc), unique = False, nullable = False)
+    event_planner = Column(Integer, ForeignKey("user.id"), nullable = False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "time": self.time,
+            "event_planner": self.event_planner
         }
